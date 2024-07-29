c5fc2fc3d44c9159b7eccb4a59bc968ddfa93b5a
Adil Shaikh <shaikhadil.dev@gmail.com>
2024-07-29 20:30:50 +0530

    fixed an import bug

--- src/pages/Syllabus.jsx
+++ src/pages/Syllabus.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 import Navbar from "../components/Navbar";
-import { removeFromTask } from "../../../server/controllers/syllabus";
 import { Link } from "react-router-dom";
 
 const Syllabus = () => {
